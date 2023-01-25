fae001895357f5a5484a5718419dab92bac0da44
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-01-25 15:03:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import VClickCounter from './components/VClickCounter.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <VClickCounter />
+</template>
+
+<style scoped>
+.logo {
+  height: 5em;
+  padding: 1em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/VClickCounter.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <div class="card">{{ count }}</div>
+    <button type="button" class="counter-button click-button" @click="count++">Click</button>
+    <button type="button" class="counter-button reset-button" @click="count=0">Reset</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .card{
+    min-width: 300px;
+    height: 200px;
+    font-size: 160px;
+    padding: 25px;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 8px solid #fff017c4;
+    margin: 15px;
+  }
+    .counter-button {
+    width: 250px;
+    height: 80px;
+    font-family: Roboto, sans-serif;
+    font-weight: bold;
+    font-size: 35px;
+    color: white;
+    border: 2px solid white;
+    border-radius: 15px;
+    margin: 5px;
+    cursor: pointer;
+    }
+
+    .counter-button:hover {
+      background-color: #00471b;
+    }
+
+    .click-button {
+      background-color: #41B883;
+    }
+
+    .reset-button {
+      background-color: #bd34fe;
+    }
+</style>
